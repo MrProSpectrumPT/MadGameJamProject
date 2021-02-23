a9c4b211bb01c5762b82a2df740f658dc3b7d232
MrProSpectrumPT <64928908+MrProSpectrumPT@users.noreply.github.com>
2021-02-23 04:18:47 +0000

    Art Update 1.0.2
     - Added Video Options
     - Added Audio Options
     - Working Audio Options
     - New Audio:
       - Button Test Sound (Effects)
       - Background Test Sound (Music)

---
+++ Mad GameJam Game/Assets/Audio/SetVolumeEffects.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolumeEffects : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+
+    public void SetLevel (float sliderValue)
+
+    {
+        mixer.SetFloat ("EffectsVolume", Mathf.Log10 (sliderValue) * 20);
+    }
+
+}
---
+++ Mad GameJam Game/Assets/Audio/SetVolumeMusic.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolumeMusic : MonoBehaviour
+{
+    
+    public AudioMixer mixer;
+
+    public void SetLevel (float sliderValue)
+
+    {
+        mixer.SetFloat ("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+    }
+
+}
