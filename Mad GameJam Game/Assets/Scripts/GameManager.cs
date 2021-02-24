ab1b8dac38302afba79288ef9a58ed8c9aa4ae3b
Braz <rodrigobrazproff@gmail.com>
2021-02-24 18:59:25 +0000

    DragObject not Finish testing

--- Mad GameJam Game/Assets/Scripts/GameManager.cs
+++ Mad GameJam Game/Assets/Scripts/GameManager.cs
@@ -6,8 +6,15 @@ public class GameManager : MonoBehaviour
 {
     public static GameObject instance;
 
-    [SerializeField]
     public GameObject groundPrefabParticle;
+
+    public LayerMask canDragLayer;
+    private Vector3 currVel;
+    private RaycastHit2D hit;
+    private bool canDrag;
+    private Vector2 mousePos;
+
+    public float mouseForce = 10f;
     void Awake()
     {
         if(instance == null)
@@ -26,6 +33,7 @@ public class GameManager : MonoBehaviour
     public void Update()
     {
         GameObject[] tmp_obj = GameObject.FindGameObjectsWithTag("particles");
+        mousePos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         if (tmp_obj.Length > 0)
         {
@@ -36,7 +44,26 @@ public class GameManager : MonoBehaviour
                     Destroy(ps);
                 }
             }
-        }   
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.forward, 100f, canDragLayer);
+            if(hit.collider.gameObject != null) canDrag = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            canDrag = false;
+            hit.collider.GetComponent<Rigidbody2D>().AddForce((mousePos - (Vector2)hit.collider.gameObject.transform.position) * mouseForce, ForceMode2D.Impulse);
+            hit.collider.GetComponent<Rigidbody2D>().gravityScale = 1.5f;
+        }
+
+        if (canDrag)
+        {
+            Debug.Log("hey");
+            hit.collider.GetComponent<Rigidbody2D>().gravityScale = 0.5f;
+            //hit.collider.GetComponent<Rigidbody2D>().velocity = 1f;
+        }
 
     }
 
