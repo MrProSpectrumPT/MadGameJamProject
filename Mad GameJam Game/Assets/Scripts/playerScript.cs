75bac5d87cc7b25805e72bea0dd1a0175f96ca68
Braz <rodrigobrazproff@gmail.com>
2021-02-26 21:12:46 +0000

    abc

--- Mad GameJam Game/Assets/Scripts/playerScript.cs
+++ Mad GameJam Game/Assets/Scripts/playerScript.cs
@@ -13,22 +13,61 @@ public class playerScript : MonoBehaviour
     public bool groundCheck;
 
     private Rigidbody2D rb;
+<<<<<<< Updated upstream
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+=======
+
+    //control Vida
+    public Slider slider;
+    public float maxVida = 100.0f;
+    public static float vidaAtual;
+
+    public float moveX;
+    private bool facingRight;
+    public bool isJumping;
+
+    private Animator anim;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        //Set Vida
+        vidaAtual = maxVida;
+        slider.maxValue = maxVida;
+        slider.value = maxVida;
+>>>>>>> Stashed changes
     }
 
    
     void Update()
     {
-        groundCheck = Physics2D.OverlapCircle(groundPosCheck.position, 0.5f, ground);
+        moveX = Input.GetAxis("Horizontal");
+        groundCheck = Physics2D.OverlapCircle(groundPosCheck.position, 0.05f, ground);
 
-        if (Input.GetKeyDown(KeyCode.Space) && groundCheck)
+        if (Input.GetKeyDown(KeyCode.Space) && groundCheck && !isJumping && rb.velocity.y == 0)
         {
             rb.AddForce(Vector3.up * jumpForce);
+            isJumping = true;
+            anim.SetBool("isJumping", true);
+            anim.SetBool("isFalling", false);
+        }else if ()
+        {
+
+        }
+        else if (groundCheck && isJumping)
+        {
+            isJumping = false;
         }
 
-        float moveX = Input.GetAxis("Horizontal");
+        if (moveX > 0 && facingRight) Flip();
+
+        else if (moveX < 0 && !facingRight) Flip();
+        
+
         rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
     }
 
@@ -40,5 +79,11 @@ public class playerScript : MonoBehaviour
             Debug.Log("enter");
             GameManager.instance.GetComponent<GameManager>().instanceGroundColision(groundPosCheck);
         }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
     }
 }
