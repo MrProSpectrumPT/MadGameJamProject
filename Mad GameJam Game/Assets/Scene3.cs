3516231e6ecff3a6ad81d2d9f50157a12c980976
Pectron <pedropassos2001@gmail.com>
2021-02-27 22:03:59 +0000

    LEVEL3
    QUASE DONE

--- Mad GameJam Game/Assets/Scene3.cs
+++ Mad GameJam Game/Assets/Scene3.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Scene3 : MonoBehaviour
 {
 
@@ -9,12 +10,25 @@ public class Scene3 : MonoBehaviour
     public static int numKills;
 
     public static bool primeiraSCENE;
+    float tempoPrim;
+
     public static bool segundaSCENE;
+    public static bool podeSegunda;
+
+    private GameObject ui;
+    GameObject barras;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ui = GameObject.Find("UI");
+        barras = GameObject.Find("UI").transform.Find("BB").gameObject;
+
+        tempoPrim = 0;
+
+        primeiraSCENE = false;
+        segundaSCENE = false;
+        podeSegunda = false;
     }
 
     // Update is called once per frame
@@ -23,14 +37,39 @@ public class Scene3 : MonoBehaviour
         numKills = status.countDeadEnemys;
 
         //ativado no player trigger se kills > 6
-        if(primeiraSCENE == true){
-            Debug.Log("morte");
+        if(primeiraSCENE == true && podeSegunda == false){
+
+            barras.SetActive(true);
+            tempoPrim += Time.deltaTime;
+            
+            GameObject.Find("Player").GetComponent<playerScript>().speed = 0f;
+
+            //CAMERA
+            GameObject.Find("Main Camera").GetComponent<Camera>().orthographicSize = 3.5f;
+            GameObject.Find("Main Camera").GetComponent<Camera>().GetComponent<cameraControl>().offset = new Vector3(0.2f,GameObject.Find("Main Camera").GetComponent<Camera>().GetComponent<cameraControl>().offset.y,GameObject.Find("Main Camera").GetComponent<Camera>().GetComponent<cameraControl>().offset.z);
+
+            GameObject.Find("UI").transform.Find("UI-GAME").gameObject.SetActive(false);
+            //GameObject.Find("Enemys").gameObject.SetActive(false);
+
+
+            if(tempoPrim > 5){
+                barras.SetActive(false);
+               // GameObject.Find("Enemys").gameObject.SetActive(true);
+                GameObject.Find("UI").transform.Find("UI-GAME").gameObject.SetActive(true);
+                GameObject.Find("UI").transform.Find("UI-GAME").gameObject.transform.Find("MyMission").GetComponent<UnityEngine.UI.Text>().text = "...vai para casa...";
+
+                //CAMERA
+                GameObject.Find("Main Camera").GetComponent<Camera>().orthographicSize = 5f;
+                GameObject.Find("Main Camera").GetComponent<Camera>().GetComponent<cameraControl>().offset = new Vector3(0,GameObject.Find("Main Camera").GetComponent<Camera>().GetComponent<cameraControl>().offset.y,GameObject.Find("Main Camera").GetComponent<Camera>().GetComponent<cameraControl>().offset.z);
+                
+                GameObject.Find("Player").GetComponent<playerScript>().speed = 10f;
+                podeSegunda = true;
+            }
         }
 
-        //ativado no player se primeiraSCENE == true
-        if(segundaSCENE == true){
-
+        //ativado no player se podeSegunda == true
+        if(segundaSCENE == false){
+            GameObject.Find("LoadLevel").GetComponent<loadlevel>().loadScene(4);
         }
-
     }
 }
