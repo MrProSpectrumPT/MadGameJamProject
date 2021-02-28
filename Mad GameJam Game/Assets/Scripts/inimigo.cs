72c0824b200263fc3ebb548c16c3b3337a468da3
Pectron <pedropassos2001@gmail.com>
2021-02-28 00:50:43 +0000

    Inimigo

--- Mad GameJam Game/Assets/Scripts/inimigo.cs
+++ Mad GameJam Game/Assets/Scripts/inimigo.cs
@@ -48,7 +48,7 @@ public class inimigo : MonoBehaviour
     {
         playerAtacou = true;
         player = GameObject.Find("Player");
-        speed = Random.Range(speed - 0.1f, speed + 0.05f); //speed diferente
+        speed = Random.Range(speed - 0.1f, speed + 0.03f); //speed diferente
 
         virado = false;
         morreu = false;
